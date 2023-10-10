73e07d6bf70b1225ae2905d161164766f0c8b361
GustGodoi <gustavogodoi442@gmail.com>
2023-10-10 14:57:50 -0300

    Iniciando o tratamento de objetos assíncronos

--- aula1/index.js
+++ aula1/index.js
@@ -10,3 +10,4 @@ const error   = chalk.bold.red;
 console.log(correct('olá mundo.'));
 console.log(error('Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.).'));
 console.log(warning('Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.).'));
+
