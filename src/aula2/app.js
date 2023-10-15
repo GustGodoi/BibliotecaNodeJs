c83c620fea42dfa6071014ea3f5db19543782ed7
GustGodoi <gustavogodoi442@gmail.com>
2023-10-14 21:44:58 -0300

    Efetuando a possibilidade de ler diversos arquivos md em busca de links, melhorando as mensagens e resultados dados na execução do programar principal 'cli.js'

--- src/aula2/app.js
+++ src/aula2/app.js
@@ -8,7 +8,7 @@ function extraiLinks(texto) {
     const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
     const capturas = [...texto.matchAll(regex)];
     const resultados = capturas.map(captura => ({[captura[1]]: captura[2]}))
-    return resultados;
+    return resultados.length !== 0 ? resultados : 'Não há links no arquivo';
 }
 
 function trataErro(error) {
@@ -20,12 +20,10 @@ async function pegaArquivo(caminhoDoArquivo) {
     try {
         const encoding = 'utf-8';
         const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
-        console.log(extraiLinks(texto));
+        return extraiLinks(texto);
 
     } catch (error) {
         trataErro(error);
-    } finally {
-        console.log(chalk.yellow('operação concluída'));
     }
 }
 
--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,39 @@
 import pegaArquivo from './aula2/app.js';
+import fs, { lstat, lstatSync } from 'fs';
+import chalk from 'chalk';
 
 const caminho = process.argv;
-console.log(caminho[2]);
 
-pegaArquivo(caminho[2]);
+function imprimeLista(nomeDeArquivo = '',resultado) {
+    console.log(
+        chalk.yellow('Lista de links do Arquivo:'),
+        chalk.black.bgGreen(nomeDeArquivo),
+        resultado);
+}
+
+async function processaTexto(argumentos) {
+    const caminho = argumentos[2];
+
+    try {
+        fs.lstatSync(caminho);
+    } catch (error) {
+       if (error.code === 'ENOENT') {
+        console.log(chalk.red('Arquivo/diretório não encontrado.'));
+        return
+       }
+    }
+ 
+    if (fs.lstatSync(caminho).isFile()) {
+        const resultado = await pegaArquivo(caminho);
+        const nomeDeArquivo = caminho;
+        imprimeLista(nomeDeArquivo,resultado);
+    } else if (fs.lstatSync(caminho).isDirectory()){
+        const arquivos = await fs.promises.readdir(caminho);
+        arquivos.forEach(async (nomeDeArquivo) => {
+            const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`);
+            imprimeLista(nomeDeArquivo,lista);
+        })
+    }
+}
+
+processaTexto(caminho);
