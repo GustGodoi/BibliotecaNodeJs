89606a655aeac32c452e1d6ef696837b3fce50cf
GustGodoi <gustavogodoi442@gmail.com>
2023-10-15 21:33:21 -0300

    Efetuando uma bilbioteca que faz a leitura de arquivos markdown, encontra e verifica se links ainda estão no ar.

--- src/cli.js
+++ src/cli.js
@@ -1,18 +1,28 @@
 import pegaArquivo from './aula2/app.js';
+import listaValidada from './httpValidacao.js';
 import fs, { lstat, lstatSync } from 'fs';
 import chalk from 'chalk';
 
 const caminho = process.argv;
 
-function imprimeLista(nomeDeArquivo = '',resultado) {
-    console.log(
-        chalk.yellow('Lista de links do Arquivo:'),
-        chalk.black.bgGreen(nomeDeArquivo),
-        resultado);
+async function imprimeLista(valida, nomeDeArquivo = '',resultado) {
+
+    if (valida) {
+        console.log(
+            chalk.yellow('Lista Validada'),
+            chalk.black.bgGreen(nomeDeArquivo),
+            await listaValidada(resultado));
+    } else {
+        console.log(
+            chalk.yellow('Lista de links do Arquivo:'),
+            chalk.black.bgGreen(nomeDeArquivo),
+            resultado);
+    }
 }
 
 async function processaTexto(argumentos) {
     const caminho = argumentos[2];
+    const valida = argumentos[3] === '--valida';
 
     try {
         fs.lstatSync(caminho);
@@ -26,12 +36,12 @@ async function processaTexto(argumentos) {
     if (fs.lstatSync(caminho).isFile()) {
         const resultado = await pegaArquivo(caminho);
         const nomeDeArquivo = caminho;
-        imprimeLista(nomeDeArquivo,resultado);
+        imprimeLista(valida, nomeDeArquivo, resultado);
     } else if (fs.lstatSync(caminho).isDirectory()){
         const arquivos = await fs.promises.readdir(caminho);
         arquivos.forEach(async (nomeDeArquivo) => {
             const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`);
-            imprimeLista(nomeDeArquivo,lista);
+            imprimeLista(valida, nomeDeArquivo, lista);
         })
     }
 }
